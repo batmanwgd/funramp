9b948da5c817f934f73f84d48107f21b381ddd03
Kaska Miskolczi <kaska.miskolczi@flocksafety.com>
2022-07-21 08:58:58 -0400

    soc levels compared

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const withImages = require('next-images')
 const nextConfig = {
   images: {
     domains: ['flocksafety.showpad.com']
-  }
+  },
+  distDir: 'build',
 }
 
 module.exports = withPlugins([[withImages]], nextConfig)
+
