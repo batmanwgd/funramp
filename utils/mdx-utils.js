46af79b7bce2b0300c09705713fdf34d1111eec7
Kaska Miskolczi <kaska.miskolczi@Kaska-Miskolczi-VHHCP73P70.local>
2022-07-21 07:32:23 -0400

    initial setup for progress updates

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -91,3 +91,94 @@ export const getPreviousPostBySlug = (slug) => {
     slug: previousPostSlug,
   };
 };
+
+
+// Repeating Posts for Doks
+
+// DOKS_PATH is useful when you want to get the path to a specific file
+export const DOKS_PATH = path.join(process.cwd(), 'doks');
+
+// dokFilePaths is the list of all mdx files inside the POSTS_PATH directory
+export const dokFilePaths = fs
+  .readdirSync(DOKS_PATH)
+  // Only include md(x) files
+  .filter((path) => /\.mdx?$/.test(path));
+
+export const sortDoksByDate = (doks) => {
+  return doks.sort((a, b) => {
+    const aDate = new Date(a.data.date);
+    const bDate = new Date(b.data.date);
+    return bDate - aDate;
+  });
+};
+
+export const getDoks = () => {
+  let doks = dokFilePaths.map((filePath) => {
+    const source = fs.readFileSync(path.join(DOKS_PATH, filePath));
+    const { content, data } = matter(source);
+
+    return {
+      content,
+      data,
+      filePath,
+    };
+  });
+
+  doks = sortPostsByDate(doks);
+
+  return doks;
+};
+
+export const getDokBySlug = async (slug) => {
+  const dokFilePath = path.join(DOKS_PATH, `${slug}.mdx`);
+  const source = fs.readFileSync(dokFilePath);
+
+  const { content, data } = matter(source);
+
+  const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [rehypePrism],
+    },
+    scope: data,
+  });
+
+  return { mdxSource, data, dokFilePath };
+};
+
+export const getNextDokBySlug = (slug) => {
+  const doks = getDoks();
+  const currentFileName = `${slug}.mdx`;
+  const currentPost = doks.find((dok) => dok.filePath === currentFileName);
+  const currentPostIndex = doks.indexOf(currentPost);
+
+  const dok = doks[currentPostIndex - 1];
+  // no prev post found
+  if (!dok) return null;
+
+  const nextPostSlug = dok?.filePath.replace(/\.mdx?$/, '');
+
+  return {
+    title: dok.data.title,
+    slug: nextPostSlug,
+  };
+};
+
+export const getPreviousDokBySlug = (slug) => {
+  const doks = getDoks();
+  const currentFileName = `${slug}.mdx`;
+  const currentPost = doks.find((dok) => dok.filePath === currentFileName);
+  const currentPostIndex = doks.indexOf(currentPost);
+
+  const dok = doks[currentPostIndex + 1];
+  // no prev post found
+  if (!dok) return null;
+
+  const previousPostSlug = dok?.filePath.replace(/\.mdx?$/, '');
+
+  return {
+    title: dok.data.title,
+    slug: previousPostSlug,
+  };
+};
