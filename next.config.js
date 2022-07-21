0d3310c6066531974ad27db4894527b7d8503372
Kaska Miskolczi <kaska.miskolczi@flocksafety.com>
2022-07-21 07:32:56 -0400

    initial setup for progress updates

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+
+const nextConfig = {
+  images: {
+    domains: ['flocksafety.showpad.com']
+  }
+}
+
+module.exports = withPlugins([[withImages]], nextConfig)
--- themes.js
+++ themes.js
@@ -1,11 +1,11 @@
 exports.COLOR_THEMES = {
   default: {
     colors: {
-      primary: '#7D7AFF',
-      'gradient-1': '#7d7aff',
-      'gradient-2': '#2121e2',
-      'gradient-3': '#00fff0',
-      'gradient-4': '#8785FF',
+      primary: '#31D15E',
+      'gradient-1': '#31D15E',
+      'gradient-2': '#26b650',
+      'gradient-3': '#FCFCF9',
+      'gradient-4': '#26b650',
     },
   },
   bejamas: {
